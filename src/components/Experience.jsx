9a97f09492dff30d34160f3eceb1f9557aea2a10
Gajulapally, Ajay Reddy <agajulap@iu.edu>
2025-04-08 20:21:42 -0400

    Experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -35,11 +35,6 @@ const Experience = () => {
           <h5 style={{ marginTop: "4px", fontSize: "0.9rem" }}>
             Bloomington, Indiana
           </h5>
-          <hr style={{margin: "12px 0 0 0"}}></hr>
-          <p style={{ lineHeight: "1.2rem" }}>
-          Developed a comprehensive MVC solution that centralized key metrics, streamlined workflows, and significantly reduced manual effort. <br></br><br></br>
-          Coordinated with cross-functional teams to rapidly address critical issues and enhance overall system reliability through best practices and modular design.
-          </p>
       </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work experience-item"
@@ -61,11 +56,6 @@ const Experience = () => {
           <h5 style={{ marginTop: "4px", fontSize: "0.9rem" }}>
             Hyderabad, Telangana
           </h5>
-          <hr style={{margin: "12px 0 0 0"}}></hr>
-          <p style={{ lineHeight: "1.2rem" }}>
-          Collaborated on a scalable backend for MATLAB Answers that drove $2M in revenue and enhanced query resolution by identifying load bottlenecks and leveraging multi-threading to reduce response times. <br></br> <br></br>
-          Streamlined deployments using IaC with Terraform, containerization with Docker, orchestration with Kubernetes and Redis caching, and ensured zero downtime through DAPR-powered distributed messaging.
-          </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work experience-item"
@@ -87,11 +77,6 @@ const Experience = () => {
           <h5 style={{ marginTop: "4px", fontSize: "0.9rem" }}>
             Bangalore, Karnataka
           </h5>
-          <hr style={{margin: "12px 0 0 0"}}></hr>
-          <p style={{ lineHeight: "1.2rem" }}>
-          Developed an OAuth2-based SSO solution using Angular to unify login flows and streamline user registrations, while creating robust middleware for error handling and validation.<br></br><br></br>
-          Enhanced platform scalability with a microservices architecture and cloud load balancing, integrated Google Calendar sync with role-based admin panels for improved navigation, and built a web-based time tracker to automate administrative tasks.
-          </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work experience-item"
@@ -113,10 +98,6 @@ const Experience = () => {
           <h5 style={{ marginTop: "4px", fontSize: "0.9rem" }}>
             IIT Dhanbad, Jharkhand
           </h5>
-          <hr style={{margin: "12px 0 0 0"}}></hr>
-          <p style={{ lineHeight: "1.2rem" }}>
-          Mentoring students in website and app development at Cyber Labs, IIT Dhanbad not only allowed me to share practical insights but also honed my own technical and leadership skills.
-          </p>
         </VerticalTimelineElement>
       </VerticalTimeline>
     </div>
